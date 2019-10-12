9284ec0590aecf750dfd40ce535ab0b7a269dd62
aknemine <aknmeinee@gmail.com>
2019-10-12 15:11:40 +0300

    Created navbar.

---
+++ HospitalAppointmentSystem/HospitalAppointmentSystem/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using HospitalAppointmentSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalAppointmentSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            HASDatabase _context = new HASDatabase();
+            _context.City.Add(new City() { name = "ASD" });
+            return View("MainPage");
+
+        }
+    }
+}
---
+++ HospitalAppointmentSystem/HospitalAppointmentSystem/Models/HASDatabase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace HospitalAppointmentSystem.Models
+{
+    public class HASDatabase : DbContext
+    {
+        public HASDatabase():base("HASDatabase")
+        {
+
+        }
+        
+        public DbSet<Appointmment> Appointmment { get; set; }
+        public DbSet<City> City { get; set; }
+        public DbSet<Town> Town { get; set; }
+        public DbSet<Hospital> Hospital { get; set; }
+        public DbSet<Doctor> Doctor { get; set; }
+        public DbSet<Patient> Patient { get; set; }
+        public DbSet<Polyclinic> Polylinic { get; set; }
+
+        
+    }
+
+    
+}
