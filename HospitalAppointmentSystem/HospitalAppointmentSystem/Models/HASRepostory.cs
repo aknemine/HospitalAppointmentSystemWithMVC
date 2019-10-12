27c82ec743dc8dca08b5f05ef1f225126934c7f3
aknemine <aknmeinee@gmail.com>
2019-10-12 23:53:01 +0300

    Uploaded navbar

---
+++ HospitalAppointmentSystem/HospitalAppointmentSystem/Models/HASRepostory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalAppointmentSystem.Models
+{
+    public class HASRepostory
+    {
+        public List<Appointmment> GetAppointmments()
+        {
+            HASDatabase hasDatabase = new HASDatabase();
+            return hasDatabase.Appointmment.ToList();
+        }
+    }
+}
